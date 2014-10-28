87778b4b15208e7bce41cfc73817d09031d2b859
Stijn Bourdeaux <bourdeauxstijn@gmail.com>
2014-10-28 19:57:56 +0100

    Automation Program
    In this file will be stored the hole automation program of the house.

---
+++ program/program.py
@@ -0,0 +1,35 @@
+class Program():
+
+    def __init__(self):
+
+        print "Program Loaded"
+
+    def startUp(self):
+        """
+        This function will only run on the beginning of the program.
+
+        :return:
+        """
+
+        print "Ob100"
+
+    def mainProgram(self):
+        """
+        This function run constantly on the ArdPi when there are no fault.
+
+        :return:
+        """
+
+    def failOver(self):
+        """
+        This function will run if the ArdPi go in fail modus.
+
+        :return:
+        """
+
+    def comFail(self):
+        """
+        This function wil start when there is no communication with the other modules.
+
+        :return:
+        """
